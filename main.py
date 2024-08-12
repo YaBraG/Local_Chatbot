35523f69f31513ab1de2906dcce988cc46a84e87
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-08-12 18:28:06 -0400

    idk

---
+++ main.py
@@ -0,0 +1,40 @@
+from langchain.chains import RetrievalQA
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PDFMinerLoader
+from flask import Flask
+
+app = Flask(__name__)
+
+loader = PDFMinerLoader("./University Physics with Modern Physics 15th edition.pdf")
+docs = loader.load()
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=2000,
+    chunk_overlap=200
+)
+texts = text_splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings()
+
+db = FAISS.from_documents(texts, embeddings)
+
+llm = Ollama(model="llama3")
+
+chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=db.as_retriever()
+)
+
+question = "Can you please summarize page 100"
+result = chain.invoke({"query": question})
+
+@app.route('/')
+def index():
+    return result['result']
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ text.py
@@ -0,0 +1,33 @@
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_ollama import ChatOllama
+from langchain.docstore.document import Document
+
+llm = Ollama(model="llama3")
+llm = ChatOllama(model = "llama3",temperature=0,#other parameters...
+                )
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+documents = [
+    Document(page_content="Rene likes soft boiled eggs.",metadata={"id":0})
+]
+
+vector_store = Chroma.from_documents(documents,embedding=embeddings)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(search_kwargs={"k": 1})
+)
+
+queries = input("Enter Prompt: ")
+
+
+response = qa_chain.run(queries)
+print(f"Query: {queries}\nResponse : {response}\n")
+
+# message = [("system","You are a helpful assistant that translates English to French. Translate the user sentence."),("human","I love the pink")]
+# ai_msg = llm.invoke(message)
+# print(ai_msg)

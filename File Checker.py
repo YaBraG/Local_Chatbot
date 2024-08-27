4e309147129e9b0bcd8f98726ea76b3a04756b6c
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-08-27 19:13:57 -0400

    idk

--- File Checker.py
+++ File Checker.py
@@ -1,7 +1,7 @@
 from os import path
 from glob import glob
 
-dir_path = "Chinga tu madre"
+dir_path_name = "Chinga tu madre"
 # Iterate over all items in the directory and create a list of pathlib.PosixPath entries each detailing a path to a .txt training file
 #.txt files are expected to be numered
 def get_training_files(dr,ext):
@@ -11,13 +11,13 @@ def get_training_files(dr,ext):
 print ("\033c")
 
 def killYourself():
-   training_set = get_training_files(dir_path,"pdf")
+   training_set = get_training_files(dir_path_name,"pdf")
    training_set2=[]
    for i in training_set:
-      training_set2.append(i.replace(f'{dir_path}\\',''))
+      training_set2.append(i.replace(f'{dir_path_name}\\',''))
    return training_set2
 
-print (training_set2)
+print ( killYourself())
 
 try:
    previousTS = killYourself()
@@ -25,4 +25,11 @@ try:
       currentTS = killYourself()
       for i in currentTS:
          if i in previousTS:
+            pass
+         else:
+            newPDF = i
+            print(newPDF)
+      previousTS = currentTS
             
+except KeyboardInterrupt:
+   print("Me fui")

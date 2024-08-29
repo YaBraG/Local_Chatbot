f93372ededbe9bcb8b13afbc729a116f1d8daabb
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-08-28 21:36:09 -0400

    idk

--- File Checker.py
+++ File Checker.py
@@ -2,13 +2,10 @@ from os import path
 from glob import glob
 
 dir_path_name = "Data"
-# Iterate over all items in the directory and create a list of pathlib.PosixPath entries each detailing a path to a .txt training file
-#.txt files are expected to be numered
+
 def get_training_files(dr,ext):
    training_files = glob(path.join(dr,"*.{}".format(ext)))
    return training_files
-
-print ("\033c")
 
 def killYourself():
    training_set = get_training_files(dir_path_name,"pdf")
--- text.py
+++ text.py
@@ -1,6 +1,7 @@
+from os import path
+from glob import glob
 from langchain_ollama import ChatOllama
-from langchain.chains import ConversationalRetrievalChain
-from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
@@ -11,20 +12,6 @@ print("\033c")
 # Cause I want to
 text = [10, 70, 117, 99, 107, 32, 89, 111, 117]
 
-template = """Answer the question in your own words as truthfully as possible from the context given to you.
-If you do not know the answer to the question, simply respond with "I don't know. Can you ask another question".
-If questions are asked where there is no relevant context available, simply respond with "I don't know. Please ask a question relevant to the documents"
-Context: {context}
-
-
-{chat_history}
-Human: {question}
-Assistant:"""
-
-prompt = PromptTemplate(
-    input_variables=["context", "chat_history", "question"], template=template
-)
-
 # Initialize the Llama 3 model
 llm = ChatOllama(model = "llama3",temperature=0)
 
@@ -32,18 +19,20 @@ llm = ChatOllama(model = "llama3",temperature=0)
 embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 # Load PDF
-loader = PyPDFLoader(r"C:\Users\elicona\OneDrive - Miami Dade College\Documents\GitHub\Local_Chatbot\Chinga tu madre\ai_adoption_framework_whitepaper.pdf")
+file_path = ("Data\\ai_adoption_framework_whitepaper.pdf")
+loader = PyPDFLoader(
+    file_path = file_path,
+    extract_images = True
+    )
 docs = loader.load()
 
 # Create Chroma vector store
 vector_store = Chroma.from_documents(docs,embedding=embeddings)
 
 # Load the QA chain
-qa_chain = ConversationalRetrievalChain.from_llm(
+qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
     retriever=vector_store.as_retriever(),
-    return_source_documents=True,
-    combine_docs_chain_kwargs={'prompt': prompt}
 )
 
 # Clear screen to only display Prompt and Response

b5ae6bcd8061fac17ce25557a3bbd22697eb3f33
YaBraG <92254077+YaBraG@users.noreply.github.com>
2024-11-15 16:42:00 -0500

    Update CUDAcheck.py

--- CUDAcheck.py
+++ CUDAcheck.py
@@ -1,6 +1,7 @@
 import torch
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-torch.cuda.is_available()
-x = torch.rand(5, 3)
-print(x)
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(device)
+# torch.cuda.is_available()
+# x = torch.rand(5, 3)
+# print(x)
+torch.cuda.empty_cache()
